4612b0d2c5eee259e8e22d46f1d71ce0a9ba3d0c
Nicolas <nszchagas@gmail.com>
2022-11-23 19:11:56 -0300

    End of formativa

---
+++ trabalho1/minhalib.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int imprime()
+{
+    printf("Mensagem teste\n");
+    return 1;
+}
+
+// int calcula(int a, int b)
+// {
+//     return a + b;
+// }
+
+int trigo(double a, double *b)
+{
+    return a;
+}
